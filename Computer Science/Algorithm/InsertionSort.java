5e383d257374a159bbebaa38eb43def086050f4b
castlecowrice <swh98@kaist.ac.kr>
2024-03-26 23:24:11 +0900

    Update InsertionSort.java

--- Computer Science/Algorithm/InsertionSort.java
+++ Computer Science/Algorithm/InsertionSort.java
@@ -1,24 +1,20 @@
-public class InsertionSort {
-    
-    static int[] arr = {10, 2, 6, 4, 3, 7, 5};
-    
-    public static void insertionSort(int[] arr) {
-        for(int i = 1; i < arr.length; i++) {
-            int num = arr[i]; // 기준
-            int aux = i - 1; // 비교대상
-            
-            while(aux >= 0 && num < arr[aux]) {
-                arr[aux+1] = arr[aux];
-                aux--;
-            }
-            arr[aux+1] = num;
-        }
-    }
-    
-    public static void main(String[] args) {
-        
-        insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-    
+void insertionSort(int[] arr)
+{
+   for(int i = 1 ; i < arr.length ; i++) {
+      int temp = arr[i];
+      int j = i - 1;
+      while( j >= 0 && arr[j] > temp ) {
+         arr[j+1] = arr[j];
+         j--;
+      }
+      arr[j+1] = temp;
+   }
+   System.out.println(Arrays.toString(arr));
 }
+
+/* 
+
+시간복잡도 : O(n) ~ O(n^2)
+공간복잡도 : O(1)
+
+*/
