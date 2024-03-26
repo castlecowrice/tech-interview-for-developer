71f01d5b502fed035523550b7e44f4dd9bc48d32
castlecowrice <swh98@kaist.ac.kr>
2024-03-26 23:16:16 +0900

    Update BubbleSort.java

--- Computer Science/Algorithm/BubbleSort.java
+++ Computer Science/Algorithm/BubbleSort.java
@@ -1,13 +1,20 @@
 void bubbleSort(int[] arr) {
-    int temp = 0;
-	for(int i = 0; i < arr.length; i++) {
-		for(int j= 1 ; j < arr.length-i; j++) {
-			if(arr[j-1] > arr[j]) {
-				temp = arr[j-1];
-				arr[j-1] = arr[j];
-				arr[j] = temp;
+	int n = arr.length;
+	for(int i = 1; i < n; i++) {
+		for(int j = 0; j < n-i; j++) {
+			if(arr[j] > arr[j+1]) {
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
 			}
 		}
 	}
 	System.out.println(Arrays.toString(arr));
 }
+
+/*
+
+시간복잡도 : O(n^2)
+공간복잡도 : O(1)
+
+*/
